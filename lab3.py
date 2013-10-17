012d5456a975b34995eaeaf78389e0b45b0a14a1
adazac10 <adazac10@ccm-2610-e2.evergreen.edu>
2013-10-17 16:41:12 -0700

    Lab3

---
+++ lab3.py
@@ -0,0 +1,31 @@
+def fib(n):
+        if n == 0 or n == 1:
+            return 1
+        return fib(n-1) + fib(n-2)
+            
+# def testfib(n):
+#       for i in range(n+1):
+#              print ('fib of, i, '-', fib(i)')
+                
+
+print fib(6) 
+
+
+
+
+series = 'sum'
+n =6
+
+def lab3fib():
+    if series == 'fibonacci':
+        return fib(n)
+    elif series == 'sum':
+        return sum(range(0, 3 * n + 1, 3))
+    else:
+        return 0
+        
+        
+print lab3fib() + 1
+
+                
+              
